07495229ad986c5e44aa4650d54ae81dda9fee69
venkatesh <venkatesh.vanagala1989@gmail.com>
2016-05-12 13:23:34 +0530

    data table creation script

--- conf/database/1.sql
+++ conf/database/1.sql
@@ -15,6 +15,13 @@ create table em_user (
   created_by                varchar(255),
   date_created              timestamp,
 constraint pk_em_user primary key (user_id));
+
+
+create table person (
+  id BIGINT NOT NULL PRIMARY KEY,
+  name` TEXT NOT NULL
+)
+
   
   
 insert into em_user(display_name, user_name, email, password, mobile, first_name, last_name, gender, age, role, last_updated, updated_by, created_by, date_created) 
@@ -29,3 +36,7 @@ values('Srinivas', 'srinivask', 'srinivas.kummari@imaginea.com', 'password', '90
 insert into em_user(display_name, user_name, email, password, mobile, first_name, last_name, gender, age, role, last_updated, updated_by, created_by, date_created) 
 values('Vinod', 'vinodk', 'vinod.kakarla@imaginea.com', 'password', '9030646406', 'Vinod', 'Kakarla', 'Male', 26, 'Principle Engineer', '2016-04-03 01:15:06', 'Vinod', 'Vinod', '2016-04-03 01:05:06');
 
+
+insert into person values(1, "Person1");
+
+insert into person values(2, "Person2");
